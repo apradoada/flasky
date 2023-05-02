23ae3eaa0c5a5b2874f45d34c9c9c2de3b6fec8d
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2023-05-02 10:31:44 -0700

    Added get one cat, update one cat, and delete one cat routes to cat_routes.py

--- app/cat_routes.py
+++ app/cat_routes.py
@@ -37,21 +37,56 @@ def get_all_cats():
 
     return jsonify(results)
 
-# # GET /cats/<id>
-# @bp.route("/<id>", methods=["GET"])
-# def handle_cat(id):
-#     cat = validate_cat(id)
+# GET /cats/<id>
+@cats_bp.route("/<id>", methods=["GET"])
+def handle_cat(id):
+    cat = validate_cat(id)
+    cat_dict = dict(
+                id=cat.id, 
+                name=cat.name, 
+                color=cat.color, 
+                personality=cat.personality
+            )
 
-#     return cat.to_dict()
+    # If we don't specify a status code, Flask will default to 200 OK.
+    # We can wrap `cat_dict`` in `jsonify``, but as a dictionary we
+    # don't need to for Flask to understand how to format the response.
+    return cat_dict
 
-# def validate_cat(id):
-#     try:
-#         id = int(id)
-#     except:
-#         abort(make_response({"message":f"cat {id} is invalid"}, 400))
+# PUT /cats/<id>
+@cats_bp.route("/<id>", methods=["PUT"])
+def replace_cat(id):
+    cat_data = request.get_json()
+    cat_to_update = validate_cat(id)
+
+    cat_to_update.name = cat_data["name"]
+    cat_to_update.color = cat_data["color"]
+    cat_to_update.personality = cat_data["personality"]
+    db.session.commit()
+
+    return make_response(f"Cat {cat_to_update.name} updated", 200)
+
+# DELETE /cats/<id>
+@cats_bp.route("/<id>", methods=["DELETE"])
+def delete_cat_by_id(id):
+    cat_to_delete = validate_cat(id)
+    db.session.delete(cat_to_delete)
+    db.session.commit()
+
+    message = f"Cat {cat_to_delete.name} deleted"
+    return make_response(message, 200)
+
+def validate_cat(id):
+    try:
+        id = int(id)
+    except:
+        message = f"cat {id} is invalid"
+        abort(make_response({"message": message}, 400))
     
-#     for cat in cats:
-#         if cat.id == id:
-#             return cat
-    
-#     abort(make_response({"message":f"cat {id} not found"}, 404))
+    cat = Cat.query.get(id)
+
+    if not cat:
+        message = f"cat {id} not found"
+        abort(make_response({"message": message}, 404))
+
+    return cat
