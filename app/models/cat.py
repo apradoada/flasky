320134e194e0ea512debe021411902711d761c1d
apradoada <adrian.prado@adadevelopersacademy.org>
2026-03-17 18:24:34 -0500

    Create Cat Model

---
+++ app/models/cat.py
@@ -0,0 +1,35 @@
+from ..db import db
+from typing import Optional
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+class Cat(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str]
+    color: Mapped[str]
+    personality: Mapped[str]
+    caretaker_id: Mapped[Optional["Caretaker"]] = relationship(back_populates="cats")
+
+    def to_dict(self):
+        result = {
+            "id": self.id, 
+            "name": self.name, 
+            "color": self.color,
+            "personality": self.personality,
+        }
+
+        if self.caretaker_id:
+            result.update({
+                "caretaker_id": self.caretaker_id,
+                "caretaker": self.caretaker.name
+            })
+
+        return result
+    
+    @classmethod
+    def from_dict(cls, cat_data):
+        return cls(name=cat_data["name"],
+                    color=cat_data["color"],
+                    personality=cat_data["personality"],
+                    caretaker_id=cat_data.get("caretaker_id", None)
+        )
