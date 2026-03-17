e2c3576a43885dca01c69b0d5fb295ceb5962c44
apradoada <adrian.prado@adadevelopersacademy.org>
2026-03-17 18:33:13 -0500

    Update configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,26 @@
 from flask import Flask
+from .db import db, migrate
+from .models.cat import Cat
+from .models.caretaker import Caretaker
+from .routes.cat_routes import bp as cats_bp
+from .routes.caretaker_routes import bp as caretakers_bp
 
-def create_app():
+import os
+
+def create_app(config = None):
     # __name__ stores the name of the module we're in
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+
+    if config:
+        app.config.update(config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    app.register_blueprint(cats_bp)
+    app.register_blueprint(caretakers_bp)
 
     return app
